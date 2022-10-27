1e58fb6cf7f61731bd53b877ffff540af557064e
NotLev1 <97649510+NotLev1@users.noreply.github.com>
2022-10-27 15:21:56 +0800

    Add files via upload

---
+++ FrmBasicThread.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LaudicoThread
+{
+    public partial class FrmBasicThread : Form
+    {
+        Thread threadA = new Thread(MyThreadClass.Thread1);
+        Thread threadB = new Thread(MyThreadClass.Thread1);
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            threadA.Name = "Thread A Process";
+            threadB.Name = "Thread B Process";
+
+            threadA.Start();
+            threadB.Start();
+
+            threadB.Join();
+            threadB.Join();
+
+            lblStart.Text = "-End Of Thread-";
+            Console.WriteLine("-End of Thread-");
+
+        }
+
+
+    }
+}
